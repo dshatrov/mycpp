6d3f303f1f523453041ac3df5017fd2287d9596d
Dmitry Shatrov <shatrov@gmail.com>
2012-12-30 02:02:03 +0100

    inlining for unicode funcs

--- mycpp/unicode.cpp
+++ mycpp/unicode.cpp
@@ -1,69 +1,27 @@
+/* MyCpp - MyNC C++ helper library
+ * Copyright (C) 2009-2012 Dmitry Shatrov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
 #include <mycpp/unicode.h>
 
+
 namespace MyCpp {
-
-// Unicode code points for newline characters.
-class NewlineCharacter
-{
-public:
-    enum _NewlineCharacter {
-	CR  = 0x0d,
-	LF  = 0x0a,
-	NEL = 0x85,
-	VT  = 0x0b, // not a newline character
-	FF  = 0x0c,
-	LS  = 0x2028,
-	PS  = 0x2029
-    };
-
-    _NewlineCharacter value;
-
-    operator _NewlineCharacter () const
-    {
-	return value;
-    }
-
-    NewlineCharacter (_NewlineCharacter const &value)
-    {
-	this->value = value;
-    }
-};
-
-bool unicode_isAlphanumeric (Unichar c)
-{
-    return g_unichar_isalnum (c);
-}
-
-bool unicode_isAlpha (Unichar c)
-{
-    return g_unichar_isalpha (c);
-}
-
-bool unicode_isDigit (Unichar c)
-{
-    return g_unichar_isdigit (c);
-}
-
-bool unicode_isSpace (Unichar c)
-{
-    return g_unichar_isspace (c);
-}
-
-FuzzyResult unicode_isNewline (Unichar c)
-{
-    switch (c) {
-	case NewlineCharacter::CR:
-	    return FuzzyResult::Maybe;
-	case NewlineCharacter::LF:
-	case NewlineCharacter::NEL:
-	case NewlineCharacter::FF:
-	case NewlineCharacter::LS:
-	case NewlineCharacter::PS:
-	    return FuzzyResult::Yes;
-	default:
-	    return FuzzyResult::No;
-    }
-}
 
 FuzzyResult unicode_isNewline (Iterator<Unichar const &> &char_iter,
 			       Size *ret_num_chars)
--- mycpp/unicode.h
+++ mycpp/unicode.h
@@ -1,26 +1,86 @@
+/* MyCpp - MyNC C++ helper library
+ * Copyright (C) 2009-2012 Dmitry Shatrov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
 #ifndef __MYCPP_UNICODE_H__
 #define __MYCPP_UNICODE_H__
 
-//#include <glib/gunicode.h>
+
 #include <glib.h>
 
 #include <mycpp/types.h>
 #include <mycpp/iterator.h>
 
+
 namespace MyCpp {
 
 typedef gunichar Unichar;
 
-bool unicode_isAlphanumeric (Unichar c);
+// Unicode code points for newline characters.
+namespace NewlineCharacter {
+    enum {
+	CR  = 0x0d,
+	LF  = 0x0a,
+	NEL = 0x85,
+	VT  = 0x0b, // not a newline character
+	FF  = 0x0c,
+	LS  = 0x2028,
+	PS  = 0x2029
+    };
+}
 
-bool unicode_isAlpha (Unichar c);
+static inline bool unicode_isAlphanumeric (Unichar c)
+{
+    return g_unichar_isalnum (c);
+}
 
-bool unicode_isDigit (Unichar c);
+static inline bool unicode_isAlpha (Unichar c)
+{
+    return g_unichar_isalpha (c);
+}
 
-bool unicode_isSpace (Unichar c);
+static inline bool unicode_isDigit (Unichar c)
+{
+    return g_unichar_isdigit (c);
+}
+
+static inline bool unicode_isSpace (Unichar c)
+{
+    return g_unichar_isspace (c);
+}
 
 // It's fuzzy because of CR+LF.
-FuzzyResult unicode_isNewline (Unichar c);
+static inline FuzzyResult unicode_isNewline (Unichar c)
+{
+    switch (c) {
+	case NewlineCharacter::CR:
+	    return FuzzyResult::Maybe;
+	case NewlineCharacter::LF:
+	case NewlineCharacter::NEL:
+	case NewlineCharacter::FF:
+	case NewlineCharacter::LS:
+	case NewlineCharacter::PS:
+	    return FuzzyResult::Yes;
+	default:
+	    return FuzzyResult::No;
+    }
+}
 
 // Recognizes "all possible" (according to the Unicode spec) newline combinations.
 FuzzyResult unicode_isNewline (Iterator<Unichar const &> &char_iter,
@@ -29,6 +89,7 @@ FuzzyResult unicode_isNewline (Iterator<Unichar const &> &char_iter,
 }
 
 #include <mycpp/utf8.h>
+
 
 #endif /* __MYCPP_UNICODE_H__ */
 
